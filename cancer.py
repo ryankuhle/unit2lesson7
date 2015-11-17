05a40d25876ab5795785fd87d0fa8045229d7298
Ryan Kuhle <ryan@ryankuhle.com>
2015-11-17 12:47:45 -0600

    Create cancer  function and input prompt for population

--- cancer.py
+++ cancer.py
@@ -12,15 +12,17 @@ what percentage is 80 of 1030
 Answer: 7.8%
 '''
 
-total_woman = 10000
-woman_w_cancer = int(total_woman * .01) #100
-pos_mammogram_w_cancer = int(woman_w_cancer * .8) #80
-pos_mammogram_without_cancer = int((total_woman - woman_w_cancer) * .096) #950
-total_pos_mammogram = int(pos_mammogram_without_cancer + pos_mammogram_w_cancer)
-percentage = pos_mammogram_w_cancer / float(total_pos_mammogram)
+def cancerMath(total_woman):
+    woman_w_cancer = int(total_woman * .01)
+    pos_mammogram_w_cancer = int(woman_w_cancer * .8)
+    pos_mammogram_without_cancer = int((total_woman - woman_w_cancer) * .096)
+    total_pos_mammogram = int(pos_mammogram_without_cancer + pos_mammogram_w_cancer)
+    percentage = pos_mammogram_w_cancer / float(total_pos_mammogram)
+    print "Women with cancer: %s" % woman_w_cancer
+    print "Women with cancer and positive mammogram: %s" % pos_mammogram_w_cancer
+    print "Women without cancer and positive mammogram: %s" % pos_mammogram_without_cancer
+    print "Women with positive mammogram: %s" % total_pos_mammogram
+    print "Percentage of positive mammograms with breast cancer: {0:.1%}".format(percentage)
 
-print "Women with cancer: %s" % woman_w_cancer
-print "Women with cancer and positive mammogram: %s" % pos_mammogram_w_cancer
-print "Women without cancer and positive mammogram: %s" % pos_mammogram_without_cancer
-print "Women with positive mammogram: %s" % total_pos_mammogram
-print "Percentage of positive mammograms with breast cancer: {0:.1%}".format(percentage)
+x = int(raw_input("How many total women are there? "))
+cancerMath(x)
