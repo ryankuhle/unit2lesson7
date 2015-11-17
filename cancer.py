970cb396eacc5ab27fdc9085150f4f166f796f88
Ryan Kuhle <ryan@ryankuhle.com>
2015-11-17 12:42:20 -0600

    Formula solved

--- cancer.py
+++ cancer.py
@@ -17,8 +17,10 @@ woman_w_cancer = int(total_woman * .01) #100
 pos_mammogram_w_cancer = int(woman_w_cancer * .8) #80
 pos_mammogram_without_cancer = int((total_woman - woman_w_cancer) * .096) #950
 total_pos_mammogram = int(pos_mammogram_without_cancer + pos_mammogram_w_cancer)
+percentage = pos_mammogram_w_cancer / float(total_pos_mammogram)
 
 print "Women with cancer: %s" % woman_w_cancer
 print "Women with cancer and positive mammogram: %s" % pos_mammogram_w_cancer
 print "Women without cancer and positive mammogram: %s" % pos_mammogram_without_cancer
 print "Women with positive mammogram: %s" % total_pos_mammogram
+print "Percentage of positive mammograms with breast cancer: {0:.1%}".format(percentage)
