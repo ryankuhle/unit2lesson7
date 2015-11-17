36e5ede94cba714620802db6e04a56d86a4e0cb5
Ryan Kuhle <ryan@ryankuhle.com>
2015-11-17 12:01:28 -0600

    Problem outlined

---
+++ cancer.py
@@ -0,0 +1,7 @@
+'''
+100 out of 10,000 women at age forty who participate in routine screening have breast cancer. 80 of every 100 women with breast cancer will get a positive mammogram. 950 out of 9,900 women without breast cancer will also get a positive mammogram.
+
+Solve: If 10,000 women in this age group undergo a routine screening, about what fraction of women with positive mammograms will actually have breast cancer?
+
+Answer: 7.8%
+'''
